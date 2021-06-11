037f4326dd8a5224c2f0bc11fe63655795d5d013
Sebastien St-Amour <sebastien@Sebastiens-MacBook-Pro.local>
2021-06-10 20:43:33 -0400

    Add POST stocks method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
@@ -12,20 +12,53 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from models import Stocks
-
+from exceptions import InvalidUsage
 
 @app.route('/stocks', methods=['POST', 'GET'])
 def stocks():
+    
     if request.method == 'GET':
         
+        if len(request.args) > 1:
+            raise InvalidUsage("Please specify only one of ['ticker', 'id', 'description', 'exchange', 'broker_id']")
+        
         ticker = request.args.get('ticker')
-        
         if ticker:
-            res = Stocks.query.filter_by(ticker='ticker').first()
-
-            return res if res else ''
-        
+            res = Stocks.query.filter_by(ticker=ticker).first()
+            
+            return jsonify(res) if res else ''
+    
         return Stocks.query.all()
+    
+    elif request.method == 'POST':
+
+        if not request.json:
+            raise InvalidUsage("No input provided")
+        
+        request_obj = request.get_json()
+        
+        if not 'ticker' in request_obj:
+            raise InvalidUsage("Ticker is required")
+        
+        if not 'description' in request_obj:
+            raise InvalidUsage("Company description is required")
+
+        if not 'broker_id' in request_obj:
+            raise InvalidUsage("Broker stock ID is required")
+
+        if not 'exchange' in request_obj:
+            raise InvalidUsage("Exchange is required")
+
+        stock = Stocks(
+            ticker=request_obj['ticker'],
+            description=request_obj['description'],
+            broker_id=request_obj['broker_id'],
+            exchange=request_obj['exchange'],)
+
+        db.session.add(stock)
+        db.session.commit()
+
+        return Stocks.query.filter_by(ticker=request_obj['ticker']).first()
 
 if __name__ == "__main__":
     app.run(debug=True)
