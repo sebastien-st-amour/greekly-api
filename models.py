68b2129f855ddd81fbb9cf6235bbddd0d34a6567
Sebastien <sebastien@purposeadvisorsolutions.com>
2021-06-13 19:51:33 -0400

    Add option quotes model (#6)
    * Add Option Quotes model
    
    * update representation
    
    Co-authored-by: Sebastien St-Amour <sebastien@Sebastiens-MacBook-Pro.local>

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from app import db
+from enums import OptionTypes
 from datetime import datetime
 
 class GreeklyModel(db.Model):
@@ -16,3 +17,43 @@ class Stocks(GreeklyModel):
 
     def __repr__(self):
         return f'Stocks(ticker={self.ticker}, description={self.description})'
+
+class OptionsQuotes(GreeklyModel):
+
+    OPTION_TYPES = [
+        ('C', 'Call'),
+        ('P', 'Put')
+    ]
+    __tablename__ = 'options_quotes'
+    id = db.Column(db.Integer, primary_key=True)
+    symbol = db.Column(db.String(30), unique=True)
+    type = db.Column(db.Enum(OptionTypes))
+    broker_id = db.Column(db.Integer, unique=True)
+    bid_price = db.Column(db.Numeric(precision=2), nullable=True)
+    bid_size = db.Column(db.Integer, default=0)
+    ask_price = db.Column(db.Numeric(precision=2), nullable=True)
+    ask_size = db.Column(db.Integer, default=0)
+    last_trade_price_tr_hrs = db.Column(db.Numeric(precision=2), nullable=True)
+    last_trade_price = db.Column(db.Numeric(precision=2), nullable=True)
+    last_trade_size = db.Column(db.Integer, default=0)
+    last_trade_tick = db.Column(db.String(10), nullable = True)
+    last_trade_time = db.Column(db.DateTime, nullable=True)
+    volume = db.Column(db.Integer, default=0)
+    open_price = db.Column(db.Numeric(precision=2), nullable=True)
+    high_price = db.Column(db.Numeric(precision=2), nullable=True)
+    low_price = db.Column(db.Numeric(precision=2), nullable=True)
+    volatility = db.Column(db.Numeric(precision=6), nullable=True)
+    delta = db.Column(db.Numeric(precision=6), nullable=True)
+    gamma = db.Column(db.Numeric(precision=6), nullable=True)
+    theta = db.Column(db.Numeric(precision=6), nullable=True)
+    vega = db.Column(db.Numeric(precision=6), nullable=True)
+    rho = db.Column(db.Numeric(precision=6), nullable=True)
+    open_interest = db.Column(db.Integer, default=0)
+    delay = db.Column(db.Integer, default=0)
+    is_halted = db.Column(db.Boolean)
+    vwap = db.Column(db.Numeric(precision=2), nullable=True)
+    stock_id = db.Column(db.Integer, db.ForeignKey('stocks.id'),
+        nullable=False)
+
+    def __repr__(self):
+        return f'OptionsQuotes(symbol={self.symbol})'
