28d54b1db492d7154ae532ecd1ed73d7291395df
Sebastien <sebastien@purposeadvisorsolutions.com>
2021-11-08 21:17:40 -0500

    add copilot manifest (#17)
    Co-authored-by: Sebastien St-Amour <sebastien@Sebastiens-MacBook-Pro.local>

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.9-slim-buster
 
+EXPOSE 5000
+
 WORKDIR /app
 
 COPY requirements.txt ./
--- wsgi.py
+++ wsgi.py
@@ -8,4 +8,4 @@ config = getenv('GREEKLY_APP_CONFIG')
 app = create_app(config)
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000)
