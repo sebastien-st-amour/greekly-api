1a719f78ab73e95285716515f75a180dd09fc1d7
Sebastien St-Amour <sebastien@Sebastiens-MacBook-Pro.local>
2021-11-08 21:15:42 -0500

    add copilot manifest

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
-import os
+import os, json
 
 class Config:
     DEBUG = False
     DEVELOPMENT = False
     SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
-    SQLALCHEMY_DATABASE_URI = os.getenv('DB_URI', 'sqlite:///sqlite.db')
+    GREEKLY_DB_SECRET = json.loads(os.getenv('GREEKLYAPICLUSTER_SECRET'))
+    DB_USER = GREEKLY_DB_SECRET['username']
+    DB_PASSWORD = GREEKLY_DB_SECRET['password']
+    DB_HOST = GREEKLY_DB_SECRET['host']
+    DB_PORT = GREEKLY_DB_SECRET['port']
+    DB_NAME = GREEKLY_DB_SECRET['dbname']
+    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
 
 class ProductionConfig(Config):
     pass
