2dc55003e37d4ec9777208206b5359173a29f2a0
Sebastien <sebastien@purposeadvisorsolutions.com>
2021-12-31 17:01:50 -0500

    chore/fix_tests_authentication (#26)
    Co-authored-by: Sebastien St-Amour <sebastien@Sebastiens-MacBook-Pro.local>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
+from werkzeug.security import generate_password_hash
 from greekly import create_app, db
-# from greekly.models import Stocks
+from greekly.models import Stocks, Users
 import pytest
 
 
@@ -11,8 +12,18 @@ def client():
     with app.test_client() as client:
         
         with app.app_context():
+
+            db.create_all()
+
+            # add test data to database
+            greekly_user = Users(email='greekly@test.com', password_hash=generate_password_hash('greeklyTest123'))
+            greekly_stock = Stocks(ticker='GREEK', description='Greekly Inc.', exchange='NASDAQ', broker_id=1234)
+
+            db.session.add(greekly_user)
+            db.session.add(greekly_stock)
+            db.session.commit()
             
             db.create_all()
             
-            yield client
+        yield client
 
