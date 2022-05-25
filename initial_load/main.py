e577d161ae73727379c04a79714426eea11baf08
Sebastien St-Amour <sebastien@Sebastiens-MacBook-Pro.local>
2022-05-25 18:12:27 -0400

    add initial_load back

--- initial_load/main.py
+++ initial_load/main.py
@@ -52,13 +52,14 @@ def get_questrade_instance():
         time = q.time
         print('time', time)
     except urllib.error.HTTPError as e:
-        s3 = boto3.resource('s3')
-        obj = s3.Object(getenv('AWS_S3_BUCKET'), 'questrade_refresh_token')
-        message = obj.get()['Body'].read().decode('utf-8')
-        token = message.split('=')[1]
-        print(f"TOKEN is {token}")
+        print('error:', e)
+        # s3 = boto3.resource('s3')
+        # obj = s3.Object(getenv('AWS_S3_BUCKET'), 'questrade_refresh_token')
+        # message = obj.get()['Body'].read().decode('utf-8')
+        # token = message.split('=')[1]
+        # print(f"TOKEN is {token}")
 
-        q = Questrade(token_path=getenv('TOKEN_PATH'), refresh_token=token)
+        # q = Questrade(token_path=getenv('TOKEN_PATH'), refresh_token=token)
     return q
 
 def main():
@@ -71,19 +72,22 @@ def main():
     q = get_questrade_instance()
     sql = 'SELECT broker_id, id FROM stocks'
     stock_list = pd.read_sql(sql=sql, con=greekly_uri).values.tolist()
-    for stock in stock_list: insert_option_contracts(q, stock, greekly)
+    
+    for stock in stock_list:
+        
+        insert_option_contracts(q, stock, greekly)
 
     # PUBLISH OPTION IDS TO SNS
-    sql = 'SELECT broker_id FROM option_contracts WHERE expiration_date > CURRENT_DATE'
-    client = boto3.client('sns', region_name='ca-central-1')
-    arns = json.loads(getenv('COPILOT_SNS_TOPIC_ARNS'))
-    topic_arn = arns.get("broker-option-ids")
+    # sql = 'SELECT broker_id FROM option_contracts WHERE expiration_date > CURRENT_DATE'
+    # client = boto3.client('sns', region_name='ca-central-1')
+    # arns = json.loads(getenv('COPILOT_SNS_TOPIC_ARNS'))
+    # topic_arn = arns.get("broker-option-ids")
 
-    for chunk in pd.read_sql(sql, con=greekly_uri, chunksize=100):
+    # for chunk in pd.read_sql(sql, con=greekly_uri, chunksize=100):
 
-        option_ids = chunk.broker_id.to_list()
-        client.publish(TopicArn=topic_arn, 
-                       Message=json.dumps(option_ids))
+    #     option_ids = chunk.broker_id.to_list()
+    #     client.publish(TopicArn=topic_arn, 
+    #                    Message=json.dumps(option_ids))
 
 if __name__ == '__main__':
     
